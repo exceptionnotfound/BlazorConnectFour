243c2bf2b1156ea9ed17159cb089b041e6c93f48
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2019-10-27 11:29:19 -0700

    Add project files.

---
+++ BlazorConnectFour/Data/GameBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorConnectFour.Data
+{
+    public class GameBoard
+    {
+        public  GamePiece[,] Board { get; set; }
+
+
+        public GameBoard()
+        {
+            Board = new GamePiece[7, 6];
+           
+            //Populate the Board with blank pieces
+            for(int i = 0; i <= 6; i++)
+            {
+                for(int j = 0; j <= 5; j++)
+                {
+                    Board[i, j] = new GamePiece(PieceColor.Blank);
+                }
+            }
+        }
+    }
+}
---
+++ BlazorConnectFour/Data/GamePiece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorConnectFour.Data
+{
+    public class GamePiece
+    {
+        public PieceColor Color;
+
+        public GamePiece()
+        {
+            Color = PieceColor.Blank;
+        }
+
+        public GamePiece(PieceColor color)
+        {
+            Color = color;
+        }
+    }
+}
